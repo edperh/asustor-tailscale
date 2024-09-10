530a739b40619623f0a8f0718ccaa838ace24567
Charles Porth <charlieporth1@gmail.com>
2024-09-10 09:10:22 -0500

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,10 +2,14 @@
 
 chmod +x ./scripts/apkg-tools_py3.py
 
+rm -rf tailscale_*.tgz
+rm -rf tailscale_*.apk
+rm -rf tailscale_*/
+rm apk/bin/tailscale*
+
 RELEASE_VERSION=$(curl --silent "https://api.github.com/repos/tailscale/tailscale/tags" | jq '.[] | select(.name | endswith("pre") | not)' | jq -r '.name' | head -n +1)
 NAME=tailscale_${RELEASE_VERSION}_${ARCH}
 
-rm apk/bin/tailscale*
 
 if [[ -f ${NAME}.tgz ]]; then
 	rm ${NAME}.tgz
